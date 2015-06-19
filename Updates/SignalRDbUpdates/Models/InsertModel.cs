02a9b9afd624d76210108515acb4b2d1a014b902
Erik Richter <ERichter_H@Dev02.bsecode.inc>
2015-06-19 09:15:25 -0500

    SignalR Notification Update

---
+++ Updates/SignalRDbUpdates/Models/InsertModel.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+//using System.Data.SqlClient;
+//using System.Linq;
+//using System.Web;
+
+//namespace SignalRDbUpdates.Models
+//{
+//    public class InsertModel
+//    {
+//        [Required]
+//        [Display(Update = "Update:")]
+//        public string Update { get; set; }
+       
+//        public int Insert(string _update)
+//        {
+//            SqlConnection cn = new SqlConnection(@"Data Source=SQL;User Id=Hydrogen;Password=Codeflange4life1;DataBase=Hydro_practice");
+//            SqlCommand cmd = new SqlCommand("Insert Into Messages(Message)Values('"+_update+"')", cn);
+//            cn.Open();
+//            return cmd.ExecuteNonQuery();
+//        }
+//    }
+//}
+//
