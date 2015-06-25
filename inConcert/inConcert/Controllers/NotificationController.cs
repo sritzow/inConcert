0371b82fa8d2cf6ba7cb03d9e3af65246a83f771
Erik Richter <ERichter_H@Dev02.bsecode.inc>
2015-06-25 12:09:16 -0500

    Notification Partial View

--- inConcert/inConcert/Controllers/NotificationController.cs
+++ inConcert/inConcert/Controllers/NotificationController.cs
@@ -13,6 +13,11 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
 
+
+
+
+
+
 namespace inConcert.Controllers
 {
 
@@ -35,13 +40,99 @@ namespace inConcert.Controllers
                 x.TimeStamp = (DateTime)row[2];
                 Notify.Update.Insert(0,x);
             }
-                        
-          
-
-            return View("Index",Notify);
             
 
+            return View("Index",Notify);
+
         }
+
+        
+
+        //void Initialization()
+        //{
+            
+        //    SqlDependency.Start(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        //}
+
+        //void SomeMethod()
+        //{
+        //    // Assume connection is an open SqlConnection.
+
+        //    // Create a new SqlCommand object.
+        //    using (SqlCommand command = new SqlCommand(
+        //        "SELECT ID, Notification, TimeStamp FROM dbo.Notifications"))
+        //    {
+
+        //        // Create a dependency and associate it with the SqlCommand.
+        //        SqlDependency dependency = new SqlDependency(command);
+        //        // Maintain the refence in a class member.
+
+        //        // Subscribe to the SqlDependency event.
+        //        dependency.OnChange += new
+        //           OnChangeEventHandler(OnDependencyChange);
+
+        //        // Execute the command.
+        //        using (SqlDataReader reader = command.ExecuteReader())
+        //        {
+        //            // Process the DataReader.
+        //        }
+        //    }
+        //}
+
+        //// Handler method
+        //void OnDependencyChange(object sender,
+        //   SqlNotificationEventArgs e)
+        //{
+        //    // Handle the event (for example, invalidate this cache entry).
+        //    Response.Redirect(Request.RawUrl);
+        //}
+
+        //void Termination()
+        //{
+        //    SqlDependency.Stop(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        //}
+
+
+
+
+        //public IEnumerable<Notification> GetAllMessages()
+        //{
+        //    var updates = new List<Notification>();
+        //    using (var connection = new SqlConnection(_connString))
+        //    {
+        //        connection.Open();
+        //        using (var command = new SqlCommand(@"SELECT [ID], [Notification], [TimeStamp] FROM [dbo].[Notifications]", connection))
+        //        {
+        //            command.Notification = null;
+
+        //            var dependency = new SqlDependency(command);
+        //            dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+
+        //            if (connection.State == ConnectionState.Closed)
+        //                connection.Open();
+
+        //            var reader = command.ExecuteReader();
+
+        //            while (reader.Read())
+        //            {
+        //                updates.Insert(0, (item: new Notification { ID = (int)reader["ID"], notificationMessage = (string)reader["Notification"], TimeStamp = Convert.ToDateTime(reader["TimeStamp"]) }));
+        //            }
+        //        }
+
+        //    }
+        //    return updates;
+
+
+        //}
+        //private void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        //{
+        //    if (e.Type == SqlNotificationType.Change)
+        //    {
+        //        IHubContext context = GlobalHost.ConnectionManager.GetHubContext<MessagesHub>();
+        //        context.Clients.All.updateMessages();
+        //    }
+        //}
+
 
 
 
