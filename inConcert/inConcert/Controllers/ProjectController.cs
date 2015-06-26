e23087ad6bfb4aaa90d210bbfdd87337d953fddc
Spencer Ritzow <sritzow_h@dev.bse.edu>
2015-06-26 15:31:39 -0700

    Adding https

--- inConcert/inConcert/Controllers/ProjectController.cs
+++ inConcert/inConcert/Controllers/ProjectController.cs
@@ -13,6 +13,7 @@ using inConcert.Helper;
 
 namespace inConcert.Controllers
 {
+    [RequireHttps]
     public class ProjectController : Controller
     {
         private ApplicationSignInManager _signInManager;
